639d03ff21a0625ee25651a220584edf72a40195
Lukas K Jacomin <lukasjacomin@gmail.com>
2025-08-12 02:15:18 -0300

    feat: add build scripts and configuration for TypeScript compilation

---
+++ scripts/build.ts
@@ -0,0 +1,42 @@
+import Bun from "bun";
+import { renameSync } from "node:fs";
+import { join } from "node:path";
+
+const buildConfig: Bun.BuildConfig = {
+  entrypoints: ["src/index.ts"],
+  outdir: "dist",
+  target: "node",
+  sourcemap: "inline",
+  minify: false,
+  packages: "external",
+  root: "./src",
+};
+
+const esmBuild = await Bun.build({
+  ...buildConfig,
+  format: "esm",
+});
+outputLogs(esmBuild);
+
+if (esmBuild.success) {
+  renameSync(join(".", "dist", "index.js"), join(".", "dist", "index.mjs"));
+}
+
+const cjsBuild = await Bun.build({
+  ...buildConfig,
+  format: "cjs",
+});
+outputLogs(cjsBuild);
+if (cjsBuild.success) {
+  renameSync(join(".", "dist", "index.js"), join(".", "dist", "index.cjs"));
+}
+
+function outputLogs(result: Bun.BuildOutput) {
+  if (result.logs.length > 0) {
+    console.warn("Build succeeded with warnings:");
+    for (const message of result.logs) {
+      // Bun will pretty print the message object
+      console.warn(message);
+    }
+  }
+}
