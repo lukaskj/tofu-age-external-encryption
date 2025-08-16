ea2a846fc377e019fec69af266324202c087636d
Lukas K Jacomin <lukasjacomin@gmail.com>
2025-08-16 01:08:09 -0300

    feat: add version and usage commands

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ import { logger } from "./utils/defaultLogger.ts";
 async function entrypoint() {
   logger.debug("Args:", process.argv);
 
-  if (!process.argv[2]) {
-    logger.fatal("Missing option. One of [--]<encrypt|decrypt|key-provider>");
-  }
+  // if (!process.argv[2]) {
+  //   logger.fatal("Missing option. One of [--]<encrypt|decrypt|key-provider>");
+  // }
 
   switch (process.argv[2]) {
     case "--encrypt":
@@ -26,9 +26,43 @@ async function entrypoint() {
     case "key":
       await keyProvider();
       break;
+    case "version":
+    case "--version":
+    case "-v":
+      console.log(version());
+      break;
     default:
-      logger.fatal("Option not regcognized:", process.argv[2]);
+      console.log(usage());
   }
+}
+
+function version() {
+  const packageJson = require("../package.json");
+  return `Version ${packageJson.version}`;
+}
+
+function usage() {
+  const packageJson = require("../package.json");
+  console.log(`Version ${packageJson.version}`);
+  const bin = packageJson.bin;
+  let command = process.argv[1];
+  if (typeof bin === "string") {
+    command = bin;
+  } else if (typeof bin === "object") {
+    command = Object.keys(bin).at(-1) ?? command;
+  }
+
+  const usage = `Usage:
+  ${command} <encrypt|decrypt|key-provider> [options]
+  ${command} --encrypt [options]
+  ${command} --decrypt [options]
+  ${command} --key-provider [options]
+  ${command} --key [options]
+  ${command} --help
+  ${command} -h
+`;
+
+  return usage;
 }
 
 entrypoint();
